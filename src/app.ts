60cf6cad1416cff57b2ab84f0018db719e4ccda7
angelsflyinhell <azami@namespace.media>
2021-12-23 16:47:30 +0100

    Fetch route finished

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,19 @@
-import {extractUrl, fetchAnimekisa, getFile} from "./lib/Web";
-import inputReader from 'wait-console-input'
+import {extractUrl, fetchAnimekisa} from "./lib/Web";
+import express from 'express';
+import config from './config.json'
+const app = express();
 
 console.log('[COSMOEM] API is starting...')
 
-const debug = async (title: string, episode: string) => {
-    console.log('[COSMOEM] Debug started')
-    const html = await fetchAnimekisa(title, episode).then(r => {
-        return r
-    });
-    const vidUrl = extractUrl(html.data) || ''
-    console.log(vidUrl.replace('load.php', 'download'))
-}
+app.listen(config.port,() => {
+    app.get('/fetch', async (req, res) => {
+        const title = req.body.title;
+        const episode = req.body.episode;
 
-const title = inputReader.readLine("Anime? ", {
-    reInputOnError: true,
-    separator: 'enter'
+        const html = await fetchAnimekisa(title, episode).then(r => {
+            return r
+        });
+        const vidUrl = extractUrl(html.data) || ''
+        res.json({url: vidUrl.replace('load.php', 'download')})
+    })
 })
-const episode = inputReader.readLine("Episode? ", {
-    reInputOnError: true,
-    separator: 'enter'
-})
-
-debug(title, episode).then(r => {
-    console.log('[COSMOEM] Debug finished')
-});
